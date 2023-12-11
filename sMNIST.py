ee20ee91f100d91a71c62078ec28e581993bf45e
AndreaCossu <andrea.cossu@di.unipi.it>
2023-12-11 14:36:53 +0100

    Added smnist

---
+++ sMNIST.py
@@ -0,0 +1,123 @@
+from torch import nn
+import torch.nn.utils
+import numpy as np
+from utils import get_mnist_data, RON
+import argparse
+from tqdm import tqdm
+from esn import DeepReservoir
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+
+parser = argparse.ArgumentParser(description='training parameters')
+
+parser.add_argument('--n_hid', type=int, default=256,
+                    help='hidden size of recurrent net')
+parser.add_argument('--batch', type=int, default=1000,
+                    help='batch size')
+parser.add_argument('--dt', type=float, default=0.042,
+                    help='step size <dt> of the coRNN')
+parser.add_argument('--gamma', type=float, default=2.7,
+                    help='y controle parameter <gamma> of the coRNN')
+parser.add_argument('--epsilon', type=float, default=4.7,
+                    help='z controle parameter <epsilon> of the coRNN')
+parser.add_argument('--gamma_range', type=float, default=2.7,
+                    help='y controle parameter <gamma> of the coRNN')
+parser.add_argument('--epsilon_range', type=float, default=4.7,
+                    help='z controle parameter <epsilon> of the coRNN')
+parser.add_argument('--cpu', action="store_true")
+parser.add_argument('--esn', action="store_true")
+parser.add_argument('--ron', action="store_true")
+parser.add_argument('--inp_scaling', type=float, default=1.,
+                    help='ESN input scaling')
+parser.add_argument('--rho', type=float, default=0.99,
+                    help='ESN spectral radius')
+parser.add_argument('--leaky', type=float, default=1.0,
+                    help='ESN spectral radius')
+parser.add_argument('--use_test', action="store_true")
+parser.add_argument('--trials', type=int, default=1,
+                    help='How many times to run the experiment')
+parser.add_argument('--topology', type=str, default='full',
+                    choices=['full', 'band', 'lower', 'toeplitz', 'orthogonal'],
+                    help='Topology of the reservoir')
+
+main_folder = 'result'
+args = parser.parse_args()
+print(args)
+
+@torch.no_grad()
+def test(data_loader, classifier, scaler):
+    activations, ys = [], []
+    for images, labels in tqdm(data_loader):
+        images = images.to(device)
+        images = images.view(args.batch, -1).unsqueeze(-1)
+        output = model(images)[-1][0]
+        activations.append(output.cpu())
+        ys.append(labels)
+    activations = torch.cat(activations, dim=0).numpy()
+    activations = scaler.transform(activations)
+    ys = torch.cat(ys, dim=0).numpy()
+    return classifier.score(activations, ys)
+
+device = torch.device("cuda") if torch.cuda.is_available() and not args.cpu else torch.device("cpu")
+
+n_inp = 1
+n_out = 10
+
+gamma = (args.gamma - args.gamma_range / 2., args.gamma + args.gamma_range / 2.)
+epsilon = (args.epsilon - args.epsilon_range / 2., args.epsilon + args.epsilon_range / 2.)
+
+train_accs, valid_accs, test_accs = [], [], []
+for i in range(args.trials):
+    if args.esn:
+        model = DeepReservoir(n_inp, tot_units=args.n_hid, spectral_radius=args.rho,
+                              input_scaling=args.inp_scaling,
+                              connectivity_recurrent=args.n_hid,
+                              connectivity_input=args.n_hid, leaky=args.leaky).to(device)
+    elif args.ron:
+        model = RON(n_inp, args.n_hid, args.dt, gamma, epsilon, args.rho,
+                      args.inp_scaling, topology=args.topology, device=device).to(device)
+
+    else:
+        raise ValueError("Wrong model choice.")
+
+    train_loader, valid_loader, test_loader = get_mnist_data(args.batch,args.batch)
+
+    objective = nn.CrossEntropyLoss()
+
+    activations, ys = [], []
+    for images, labels in tqdm(train_loader):
+        images = images.to(device)
+        images = images.view(args.batch, -1).unsqueeze(-1)
+        output = model(images)[-1][0]
+        activations.append(output.cpu())
+        ys.append(labels)
+    activations = torch.cat(activations, dim=0).numpy()
+    ys = torch.cat(ys, dim=0).numpy()
+    scaler = preprocessing.StandardScaler().fit(activations)
+    activations = scaler.transform(activations)
+    classifier = LogisticRegression(max_iter=1000).fit(activations, ys)
+    train_acc = test(train_loader, classifier, scaler)
+    valid_acc = test(valid_loader, classifier, scaler) if not args.use_test else 0.0
+    test_acc = test(test_loader, classifier, scaler) if args.use_test else 0.0
+    train_accs.append(train_acc)
+    valid_accs.append(valid_acc)
+    test_accs.append(test_acc)
+
+if args.ron:
+    f = open(f'{main_folder}/sMNIST_log_RON.txt', 'a')
+elif args.esn:
+    f = open(f'{main_folder}/sMNIST_log_ESN.txt', 'a')
+else:
+    raise ValueError("Wrong model choice.")
+
+ar = ''
+for k, v in vars(args).items():
+    ar += f'{str(k)}: {str(v)}, '
+ar += (f'train: {[str(round(train_acc, 2)) for train_acc in train_accs]} '
+       f'valid: {[str(round(valid_acc, 2)) for valid_acc in valid_accs]} '
+       f'test: {[str(round(test_acc, 2)) for test_acc in test_accs]}' 
+       f'mean/std train: {np.mean(train_accs), np.std(train_accs)} '
+       f'mean/std valid: {np.mean(valid_accs), np.std(valid_accs)} '
+       f'mean/std test: {np.mean(test_accs), np.std(test_accs)}')
+f.write(ar + '\n')
+f.close()
---
+++ utils.py
@@ -0,0 +1,225 @@
+from scipy.integrate import odeint
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch import nn
+from esn import spectral_norm_scaling
+import torch.utils.data as data
+import torch.nn.functional as F
+
+
+class datasetforRC(data.Dataset):
+    """
+    This class assumes mydata to have the form:
+            [ (x1,y1), (x2,y2) ]
+    where xi are inputs, and yi are targets.
+    """
+
+    def __init__(self, mydata):
+        self.mydata = mydata
+
+    def __getitem__(self, idx):
+        sample = self.mydata[idx]
+        idx_inp, idx_targ = sample[0], sample[1]
+        idx_inp, idx_targ = torch.Tensor(idx_inp), torch.Tensor([idx_targ])
+        # reshape time series for torch (batch, inplength, inpdim)
+        idx_inp = idx_inp.reshape(idx_inp.shape[0], 1)
+        # one-hot encoding gives problems with scikit-learn LogisticRegression of RC models
+        return idx_inp, idx_targ
+
+    def __len__(self):
+        return len(self.mydata)
+
+
+class Adiac_dataset(data.Dataset):
+    """
+    This class assumes mydata to have the form:
+            [ (x1,y1), (x2,y2) ]
+    where xi are inputs, and yi are targets.
+    """
+
+    def __init__(self, mydata):
+        self.mydata = mydata
+
+    def __getitem__(self, idx):
+        sample = self.mydata[idx]
+        idx_inp, idx_targ = sample[0], sample[1]
+        idx_inp, idx_targ = torch.Tensor(idx_inp), torch.Tensor([idx_targ])
+        # reshape time series for torch (batch, inplength, inpdim)
+        idx_inp = idx_inp.reshape(idx_inp.shape[0], 1)
+        # one-hot encoding targets
+        idx_targ = F.one_hot(idx_targ.type(torch.int64), num_classes=37).float()
+        # reshape target for torch (batch, classes)
+        idx_targ = idx_targ.reshape(idx_targ.shape[1])
+        return idx_inp, idx_targ
+
+    def __len__(self):
+        return len(self.mydata)
+
+
+class LSTM(nn.Module):
+    def __init__(self, n_inp, n_hid, n_out):
+        super().__init__()
+        self.lstm = torch.nn.LSTM(n_inp, n_hid, batch_first=True,
+                                  num_layers=1)
+        self.readout = torch.nn.Linear(n_hid, n_out)
+
+    def forward(self, x):
+        out, h = self.lstm(x)
+        out = self.readout(out[:, -1])
+        return out
+
+
+def get_hidden_topology(n_hid, topology):
+    if topology == 'full':
+        h2h = 2 * (2 * torch.rand(n_hid, n_hid) - 1)
+    else:
+        raise ValueError("Wrong topology choice.")
+    return h2h
+
+
+class RON(nn.Module):
+    """
+    Batch-first (B, L, I)
+    """
+    def __init__(self, n_inp, n_hid, dt, gamma, epsilon, rho, input_scaling, topology='full', device='cpu'):
+        super().__init__()
+        self.n_hid = n_hid
+        self.device = device
+        self.dt = dt
+        if isinstance(gamma, tuple):
+            gamma_min, gamma_max = gamma
+            self.gamma = torch.rand(n_hid, requires_grad=False, device=device) * (gamma_max - gamma_min) + gamma_min
+        else:
+            self.gamma = gamma
+        if isinstance(epsilon, tuple):
+            eps_min, eps_max = epsilon
+            self.epsilon = torch.rand(n_hid, requires_grad=False, device=device) * (eps_max - eps_min) + eps_min
+        else:
+            self.epsilon = epsilon
+
+        h2h = get_hidden_topology(n_hid, topology)
+        h2h = spectral_norm_scaling(h2h, rho)
+        self.h2h = nn.Parameter(h2h, requires_grad=False)
+
+        # x2h = 2 * (2 * torch.rand(n_inp, self.n_hid) - 1) * input_scaling
+        # alternative init
+        x2h = torch.rand(n_inp, n_hid) * input_scaling
+        self.x2h = nn.Parameter(x2h, requires_grad=False)
+        bias = (torch.rand(n_hid) * 2 - 1) * input_scaling
+        self.bias = nn.Parameter(bias, requires_grad=False)
+
+    def cell(self, x, hy, hz):
+        hz = hz + self.dt * (torch.tanh(
+            torch.matmul(x, self.x2h) + torch.matmul(hy, self.h2h) + self.bias)
+                             - self.gamma * hy - self.epsilon * hz)
+
+        hy = hy + self.dt * hz
+        return hy, hz
+
+    def forward(self, x):
+        hy = torch.zeros(x.size(0),self.n_hid).to(self.device)
+        hz = torch.zeros(x.size(0),self.n_hid).to(self.device)
+        all_states = []
+        for t in range(x.size(1)):
+            hy, hz = self.cell(x[:, t],hy,hz)
+            all_states.append(hy)
+
+        return torch.stack(all_states, dim=1), [hy]  # list to be compatible with ESN implementation
+
+
+def get_mnist_data(bs_train,bs_test):
+    train_dataset = torchvision.datasets.MNIST(root='data/',
+                                               train=True,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
+
+    test_dataset = torchvision.datasets.MNIST(root='data/',
+                                              train=False,
+                                              transform=transforms.ToTensor())
+
+    train_dataset, valid_dataset = torch.utils.data.random_split(train_dataset, [57000,3000])
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=bs_train,
+                                               shuffle=True)
+
+    valid_loader = torch.utils.data.DataLoader(dataset=valid_dataset,
+                                              batch_size=bs_test,
+                                              shuffle=False)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=bs_test,
+                                              shuffle=False)
+
+    return train_loader, valid_loader, test_loader
+
+
+def get_Adiac_data(bs_train,bs_test, whole_train=False, RC=True):
+
+    def fromtxt_to_numpy(filename='Adiac_TRAIN.txt', valid_len=120):
+        # read the txt file
+        adiacdata = np.genfromtxt(filename, dtype='float64')
+        # create a list of lists with each line of the txt file
+        l = []
+        for i in adiacdata:
+            el = list(i)
+            while len(el) < 3:
+                el.append('a')
+            l.append(el)
+        # create a numpy array from the list of lists
+        arr = np.array(l)
+        if valid_len is None:
+            test_targets = arr[:,0]-1
+            test_series = arr[:,1:]
+            return test_series, test_targets
+        else:
+            if valid_len == 0:
+                train_targets = arr[:,0]-1
+                train_series = arr[:,1:]
+                val_targets = arr[0:0,0] # empty
+                val_series = arr[0:0,1:] # empty
+            elif valid_len > 0 :
+                train_targets = arr[:-valid_len,0]-1
+                train_series = arr[:-valid_len,1:]
+                val_targets = arr[-valid_len:,0]-1
+                val_series = arr[-valid_len:,1:]
+            return train_series, train_targets, val_series, val_targets
+
+    # Generate list of input-output pairs
+    def inp_out_pairs(data_x, data_y):
+        mydata = []
+        for i in range(len(data_y)):
+            sample = (data_x[i,:], data_y[i])
+            mydata.append(sample)
+        return mydata
+
+    # generate torch datasets
+    if whole_train:
+        valid_len = 0
+    else:
+        valid_len = 120
+    train_series, train_targets, val_series, val_targets = fromtxt_to_numpy(filename='Adiac_TRAIN.txt', valid_len=valid_len)
+    mytraindata, myvaldata = inp_out_pairs(train_series, train_targets), inp_out_pairs(val_series, val_targets)
+    if RC:
+        mytraindata, myvaldata = datasetforRC(mytraindata), datasetforRC(myvaldata)
+        test_series, test_targets = fromtxt_to_numpy(filename='Adiac_TEST.txt', valid_len=None)
+        mytestdata = inp_out_pairs(test_series, test_targets)
+        mytestdata = datasetforRC(mytestdata)
+    else:
+        mytraindata, myvaldata = Adiac_dataset(mytraindata), Adiac_dataset(myvaldata)
+        test_series, test_targets = fromtxt_to_numpy(filename='Adiac_TEST.txt', valid_len=None)
+        mytestdata = inp_out_pairs(test_series, test_targets)
+        mytestdata = Adiac_dataset(mytestdata)
+
+
+    # generate torch dataloaders
+    mytrainloader = data.DataLoader(mytraindata,
+                    batch_size=bs_train, shuffle=True, drop_last=True)
+    myvaloader = data.DataLoader(myvaldata,
+                        batch_size=bs_test, shuffle=False, drop_last=True)
+    mytestloader = data.DataLoader(mytestdata,
+                batch_size=bs_test, shuffle=False, drop_last=True)
+    return mytrainloader, myvaloader, mytestloader
