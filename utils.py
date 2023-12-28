f4ba4b3cbb163f9aecbe94966287b19dcce6aa78
andreaceni <99392616+andreaceni@users.noreply.github.com>
2023-12-28 10:27:51 +0100

    Update utils.py
    added get_sparsity function

--- utils.py
+++ utils.py
@@ -71,6 +71,13 @@ class LSTM(nn.Module):
         return out
 
 
+def get_sparsity(A):
+    n_hid = A.shape[0]
+    sparsity = 100 * (n_hid**2 - np.count_nonzero(A))/n_hid**2
+    print('sparsity: ' + str(sparsity) +'%')
+    return sparsity
+
+
 def get_hidden_topology(n_hid, topology, sparsity, scaler):
     if topology == 'full':
         h2h = 2 * (2 * torch.rand(n_hid, n_hid) - 1)
@@ -80,6 +87,7 @@ def get_hidden_topology(n_hid, topology, sparsity, scaler):
             n_zeroed_diagonals = int(sparsity * n_hid)
             for i in range(n_hid-1, n_hid - n_zeroed_diagonals - 1, -1):
                 h2h.diagonal(-i).zero_()
+        get_sparsity(h2h.numpy())
     elif topology == 'orthogonal':
         rand = torch.rand(n_hid, n_hid)
         orth = torch.linalg.qr(rand)[0]
@@ -89,6 +97,7 @@ def get_hidden_topology(n_hid, topology, sparsity, scaler):
             idxs = torch.randperm(n_hid)[:n_zeroed_rows].tolist()
             identity[idxs, idxs] = 0.
         h2h = torch.matmul(identity, orth)
+        get_sparsity(h2h.numpy())
     elif topology == 'band':
         h2h = 2*torch.rand(n_hid, n_hid)-1
         if sparsity > 0:
@@ -96,6 +105,7 @@ def get_hidden_topology(n_hid, topology, sparsity, scaler):
             for i in range(n_hid-1, n_hid - n_zeroed_diagonals - 1, -1):
                 h2h.diagonal(-i).zero_()
                 h2h.diagonal(i).zero_()
+        get_sparsity(h2h.numpy())
     elif topology == 'ring':
         # scaler = 1
         h2h = torch.zeros(n_hid, n_hid)
@@ -103,6 +113,7 @@ def get_hidden_topology(n_hid, topology, sparsity, scaler):
             h2h[i, i - 1] = 1
         h2h[0, n_hid - 1] = 1
         h2h = scaler * h2h
+        get_sparsity(h2h.numpy())
     elif topology == 'toeplitz':
         from scipy.linalg import toeplitz
         bandwidth = int(scaler)  # 5
@@ -112,6 +123,7 @@ def get_hidden_topology(n_hid, topology, sparsity, scaler):
         lowerdiagcoefs[:bandwidth] = 2 * torch.rand(bandwidth) - 1
         lowerdiagcoefs[0] = upperdiagcoefs[0]  # diagonal coefficient
         h2h = toeplitz(list(lowerdiagcoefs), list(upperdiagcoefs))
+        get_sparsity(h2h)
         h2h = torch.Tensor(h2h)
     else:
         raise ValueError("Wrong topology choice.")
